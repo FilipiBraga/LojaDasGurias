bbd8c1e6dabe291dd40e6f3a17f9a3cdf0a57c29
filipibc <filipibc@gmail.com>
2023-01-10 12:55:41 -0300

    - Criação de Entitade cliente - Criação de teste de entidade cliente

---
+++ LojaDasGurias.Domain/Entities/Cliente.cs
@@ -0,0 +1,22 @@
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+
+namespace LojaDasGurias.Domain.Entities
+{
+    public class Cliente : Notifiable<Notification>
+    {
+        public Cliente(string nome, DateTime dataDeNascimento)
+        {
+            Nome = nome;
+            DataDeNascimento = dataDeNascimento;
+
+            AddNotifications(new Contract<Cliente>()
+                .Requires()
+                .IsGreaterThan(DataDeNascimento, DateTime.MinValue, nameof(DataDeNascimento), "A data de nascimento é obrigatória")
+                .IsNotNullOrWhiteSpace(Nome, nameof(Nome), "O nome é obrigatório"));
+        }
+
+        public string Nome { get; private set; }
+        public DateTime DataDeNascimento { get; private set; }
+    }
+}
---
+++ LojaDasGurias.Tests/Entities/ClientesTests.cs
@@ -0,0 +1,37 @@
+﻿using LojaDasGurias.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LojaDasGurias.Tests.Entities
+{
+    [Collection("Mapper")]
+    public class ClientesTests
+    {
+        [Fact]
+        public void CriarCliente_Sucesso_Test()
+        {
+            var cliente = new Cliente("João", DateTime.Now);
+
+            Assert.True(cliente.IsValid);
+        }
+
+        [Fact]
+        public void CriarCliente_NomeVazio_Test()
+        {
+            var cliente = new Cliente(string.Empty, DateTime.Now);
+
+            Assert.False(cliente.IsValid);
+        }
+
+        [Fact]
+        public void CriarCliente_DataMinima_Test()
+        {
+            var cliente = new Cliente("João", DateTime.MinValue);
+
+            Assert.False(cliente.IsValid);
+        }
+    }
+}
